7cc50f96db84813b71df78e59c701d903d77fd69
Kihong Heo <kihong.heo@kaist.ac.kr>
2023-06-09 15:31:03 +0900

    update string

--- string/src/string.ml
+++ string/src/string.ml
@@ -1,13 +1,14 @@
 let opt = ref false
 let input = ref ""
 let options = [ ("-opt", Arg.Set opt, "Optimized version") ]
+let long_list n = List.init n (fun _ -> Random.int 10000000)
 let to_string_unopt lst = List.fold_left (fun s n -> s ^ string_of_int n) "" lst
 let to_string_opt lst = failwith "Not implemented"
 
 let main () =
   Arg.parse options (fun x -> input := x) "";
   let n = int_of_string !input in
-  let init_list = List.init n Fun.id in
+  let init_list = long_list n in
   let to_string = if !opt then to_string_opt else to_string_unopt in
   let to_string_list = to_string init_list in
   String.length to_string_list |> Printf.printf "%d\n"
