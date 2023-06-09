18a4d4fa3f9c4a287d095f1f4588ef1c0bfe21c5
Kihong Heo <kihong.heo@kaist.ac.kr>
2023-06-09 16:08:19 +0900

    add profiling

---
+++ profiling/src/profiling.ml
@@ -0,0 +1,17 @@
+let opt = ref false
+let input = ref ""
+let options = [ ("-opt", Arg.Set opt, "Optimized version") ]
+let long_list n = List.init n (fun _ -> Random.int 10000000)
+let to_string_unopt lst = List.fold_left (fun s n -> s ^ string_of_int n) "" lst
+let to_string_opt lst = failwith "Not implemented"
+
+let main () =
+  Arg.parse options (fun x -> input := x) "";
+  Memtrace.trace_if_requested ();
+  let n = int_of_string !input in
+  let init_list = long_list n in
+  let to_string = if !opt then to_string_opt else to_string_unopt in
+  let to_string_list = to_string init_list in
+  String.length to_string_list |> Printf.printf "%d\n"
+
+let _ = main ()
